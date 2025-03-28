afe4fce5d4204c8fead1b3d652f11bbab49acba7
David Kendall <david@rocketmakers.com>
2025-03-28 08:43:54 +0000

    feat: Added templates for submit application flows

---
+++ layouts/submitApplicationBroker/model.ts
@@ -0,0 +1,14 @@
+/**
+ * Specify required object
+ *
+ * @examples require(".").sampleData
+ */
+export interface IModel {
+  name: string;
+}
+
+export const sampleData: IModel[] = [
+  {
+    name: "name",
+  },
+];
